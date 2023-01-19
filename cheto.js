665e2975c15d6c1e703e2c6e427b651ff4d80eba
kadan-dev <61644490+abasakre@users.noreply.github.com>
2023-01-19 11:03:54 +0300

    Update cheto.js

--- cheto.js
+++ cheto.js
@@ -1,11 +1,10 @@
-
 var obj = JSON.parse($response.body);
 
-obj = {
-    "status": true,
-    "uuid": "9B5A5CD1-4286-4F1F-8421-0667D20F1A90",
-    "endtime": "2023-01-29 14:30",
-    "message":"Safe 100%"
+obj ={
+	"status": "true",
+	"uuid": "9B5A5CD1-4286-4F1F-8421-0667D20F1A90",
+	"endtime": "2023-01-19 14:30",
+	"message": "Safe 100%"
 }
 
 $done({body: JSON.stringify(obj)});
