dce7d1a583797be116226f4d5643745dfe37b3dc
kadan-dev <61644490+abasakre@users.noreply.github.com>
2023-01-20 07:56:19 +0300

    Update ip.js

--- ip.js
+++ ip.js
@@ -1,10 +1,23 @@
 var obj = JSON.parse($response.body);
 
 obj={
-    
-        "ip": "212.237.118.179",
-        "continent": "akre",
-      
-}
+        "ip": "212.237.118.39",
+        "continent": "ffffffffff",
+        "continentCode": "ffffffffffffffff",
+        "country": "IQ",
+        "countryName": "ffffffffffffffffffffffffff",
+        "subdivisions": [
+          "DA"
+        ],
+        "city": "Duhok",
+        "timezone": "Asia/Baghdad",
+        "latitude": 36.8591,
+        "longitude": 42.987,
+        "isp": "O3-fffffffffffffffffffffffffffffff",
+        "asnNumber": 206206,
+        "asnOrganization": "ffffffffffffffffffffffffffffff Net Company for Computer and Internet Ltd.",
+        "organization": "Kurdistaffffffffffffnnet",
+        "postalCode": null
+      }
 
 $done({body: JSON.stringify(obj)});
