82a930a135ce3f79f7823d14b5d03d2432fff62f
kadan-dev <61644490+abasakre@users.noreply.github.com>
2023-01-22 19:29:26 +0300

    Update cheto.js

--- cheto.js
+++ cheto.js
@@ -1,2 +1,3 @@
 var obj = `{"status":"true","uuid":"9B5A5CD1-4286-4F1F-8421-0667D20F1A90","endtime":"2023-01-30 14:30","message":"Safe 100%"}`;
-$done({body: obj});
+body = JSON.stringify(obj);
+$done({body});
