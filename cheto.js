5012bce9162e54dc0e52a739534ac6412fb7c95e
kadan-dev <61644490+abasakre@users.noreply.github.com>
2023-01-22 18:16:23 +0300

    df

--- cheto.js
+++ cheto.js
@@ -1,2 +1,2 @@
-var obj = {"status": "true","uuid": "9B5A5CD1-4286-4F1F-8421-0667D20F1A90","endtime": "2023-01-30 14:30","message": "Safe 100%"};
+var obj = `{"status": "true","uuid": "9B5A5CD1-4286-4F1F-8421-0667D20F1A90","endtime": "2023-01-30 14:30","message": "Safe 100%"}`;
 $done({body: obj});
