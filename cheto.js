02253f2e51cdc26f160aa1c6e934ac907bc9946c
kadan-dev <61644490+abasakre@users.noreply.github.com>
2023-01-22 18:30:36 +0300

    Update cheto.js

--- cheto.js
+++ cheto.js
@@ -1,2 +1,8 @@
-var obj = {"status": "true","uuid": "9B5A5CD1-4286-4F1F-8421-0667D20F1A90","endtime": "2023-01-30 14:30","message": "Safe 100%"};
-$done({body: JSON.stringify(obj)});
+var obj = `{
+	"status": "true",
+	"uuid": "9B5A5CD1-4286-4F1F-8421-0667D20F1A90",
+	"endtime": "2023-01-19 14:30",
+	"message": "Safe 100%"
+}
+`;
+$done({body: obj});
